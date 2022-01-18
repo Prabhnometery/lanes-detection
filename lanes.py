e5744e87d927c566a136f36d65225bfaec030999
Prabhnometery <pssbadwa@uwaterloo.ca>
2022-01-18 00:11:50 -0500

    converted the image to grayscale

--- lanes.py
+++ lanes.py
@@ -1,7 +1,10 @@
 import cv2
+import numpy as np
 
 # Read the image from the file 
 image = cv2.imread('test_image.jpeg')
+lane_image = np.copy(image)
+gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
 # Display Image
-cv2.imshow('result', image)
+cv2.imshow('result', gray)
 cv2.waitKey(0)
